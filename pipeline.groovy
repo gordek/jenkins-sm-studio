d7d8b83ce4a5216b37288dfb3aecc761891382a2
Gokhan Ordek <gokho@amazon.com>
2021-08-10 13:42:55 -0700

    step is added

--- pipeline.groovy
+++ pipeline.groovy
@@ -18,3 +18,51 @@ def customImageTag(String branch_name, environment) {
         return readFile("${WORKSPACE}/environments/${environment}/version.txt").trim().replaceAll(specialCharRegex, '-')
     }
 }
+
+def buildAndPush(Map args) {
+    stdout = sh(
+            returnStdout: true,
+            script: "make build_n_push_with_repo " +
+                    "FULL_BASE_IMAGE_NAME=${args.base_image_name} " +
+                    "AWS_ACCOUNT_ID=${args.account} " +
+                    "AWS_REGION=${args.region} " +
+                    "REPO=${args.repo} " +
+                    "IMAGE_TAG=${args.tag} " +
+                    "DOCKER_FILE_PATH=${args.docker_path} "
+    )
+    return "${stdout}"
+}
+
+
+pipeline {
+    agent any
+
+    parameters {
+        booleanParam(name: 'build_base', defaultValue: false, description: "build base images, only default branch")
+        booleanParam(name: 'build_gpu', defaultValue: false, description: "build gpu images, only default branch")
+        booleanParam(name: 'rebuild_all_envs', defaultValue: false, description: "Rebuild all environment images")
+        string(name: 'build_env', defaultValue: "", description: "Name of environment folder to rebuild")
+        string(name: 'BRANCH_NAME', defaultValue: "development", description: "Name of branch")
+        booleanParam(name: 'release', defaultValue: false, description: "release to Sage Maker")
+        booleanParam(name: 'STOP_ALL', defaultValue: false, description: "!!!! STOP ALL InService Apps when Release !!!!")
+    }
+    stage('Build and Push Base Image') {
+            when {
+                expression { params.build_base == true && params.BRANCH_NAME == DEFAULT_BRANCH}
+            }
+            steps {
+                script {
+                    // withCerberus([sdbPath: CERBERUS_SECRET_PATH, sdbKeys:['aws-test-account':'AWS_ACCOUNT']]) {
+                        // withAWS(role: "arn:aws:iam::${AWS_ACCOUNT}:role/${AWS_ROLE}", region: AWS_REGION) {
+                            buildAndPushBaseImage(
+                                    account: AWS_ACCOUNT,
+                                    region: AWS_REGION,
+                                    repo: ECR_BASE_IMAGE_REPOSITORY,
+                                    docker_path: "${WORKSPACE}/docker/sm-studio-base/Dockerfile"
+                            )
+                        // }
+                    // }
+                }
+            }
+        }
+}
